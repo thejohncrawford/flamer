33b58cc6436154ef337c33d1150820f240c8a6c3
Luke Loken <lukeloken@gmail.com>
2015-07-01 13:47:32 -0500

    Moved "write table" to Flame_Wrapper function Included a write_table to the Data Archive folder. These will be the files served on the LTER after our departure.

--- R/FLAMe_Wrapper.r
+++ R/FLAMe_Wrapper.r
@@ -6,14 +6,15 @@
 flame_engage=function(meta){
 #Read and merge all instrument data according to flame on intervals 
 instruments=read_instruments(Site, Date)
-write.table(instruments, file = as.character(paste(Site, Date, "raw.csv", sep="")), col.names=TRUE,row.names=F, sep=",")
 #Apply hydraulic and tau corrections.
 geodata=tau_correct(instruments)
 #Convert CO2 and CH4 to uM and %Sat units
 #numeric value (meters) computed from GpsFull$altitude in read instruments script. 
 geodata=convert_gases(geodata, meta$Elevation[1])
 #Extract Flame data from water chemistry collection times.
-write.table(geodata, file = as.character(paste(Site, Date, "corrected.csv", sep="")), col.names=TRUE,row.names=F, sep=",")
+write.table(geodata, file = as.character(paste(Site, Date, ".csv", sep="")), col.names=TRUE,row.names=F, sep=",")
+Syncfolder<-unlist(strsplit(getwd(), split="Data"))[1]
+write.table(geodata, file = as.character(paste(Syncfolder,"DataArchive/" , Site, Date, ".csv", sep="")), col.names=TRUE,row.names=F, sep=",")
 sampledata=extract_flame(geodata,Samples, SamplesPath)
 #Convert dataframe to geospatial object and export shapefile. 
 #'shapefile' has lat/lon in decimal degrees
