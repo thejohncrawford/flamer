af78ede3c7383ebe94b59c536308453f8d84cc44
Luke Loken <lukeloken@gmail.com>
2015-07-07 10:24:09 -0500

    Round all samples times to minutes. Export csvs to new files (rather than amending original csv) Export a copy of the samples csv to a common folder that will accumulate all water chem samples data.

--- R/extract_flame.R
+++ R/extract_flame.R
@@ -11,9 +11,8 @@ extract_flame=function(table, samples, SamplesPath){
   if (meta$number_of_samples[1]!=0){
   library(caTools)
   samplevars<-c ("sample_id", "event_id", "Sample.Number", "Sample.Time", "Sample.Notes")
-  samples<-samples[samplevars]
-  samples<-samples[is.na(samples$Sample.Number)==FALSE,]
-  samples$DateTime<-as.POSIXct(paste(Date,samples$Sample.Time, sep=" "), format="%Y-%m-%d %H:%M:%S", tz=as.character(meta$GPS_Timezone[1]))
+  samples<-samples[is.na(samples$Sample.Time)==FALSE,]
+  samples$DateTime<-as.POSIXct(round(as.POSIXct(paste(Date,samples$Sample.Time, sep=" "), format="%Y-%m-%d %H:%M:%S", tz=as.character(meta$GPS_Timezone[1])), "mins"))
     
   table$ltime<-as.POSIXct(round(table$ltime, "mins"))
  
@@ -23,7 +22,10 @@ extract_flame=function(table, samples, SamplesPath){
   
   Flame_times<-merge(samples, table2, by.x="DateTime", by.y="ltime" )
   
-  write.table(Flame_times, file = as.character(paste("samples/", SamplesPath, sep="")), col.names=TRUE,row.names=F, sep=",")
+  write.table(Flame_times,  file = as.character(paste(Site, Date, "_Samples", ".csv", sep="")), col.names=TRUE,row.names=F, sep=",")
+
+  folder<-unlist(strsplit(getwd(), split="Data"))[1]
+  write.table(Flame_times, file = as.character(paste(folder,"Data/WaterChemSamples/", Site, Date, "_Samples", ".csv", sep="")), col.names=TRUE,row.names=F, sep=",")
   
   return(Flame_times)
   }
