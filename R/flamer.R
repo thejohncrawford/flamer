b993f9fbf11066ad90f329fa224edea182776ae7
lukeloken <lukeloken@gmail.com>
2016-09-15 15:13:36 -0500

    Updated FLAMer to 2016 code. This code runs all other scripts to load flamer functions.

--- R/flamer.R
+++ R/flamer.R
@@ -1,4 +1,27 @@
-#New code to do some stuff
-#Test github committ
+# Read Flame Scripts
+#updated June 2016, Luke Loken
 
-#more code to do more things!
+# Main script
+source("FLAMe_Wrapper_All_Maps.R")
+
+#reading instruments
+source("suna_datetime.R")
+source("read_instruments.R")
+source("flame_cut.R")
+
+#correcting data
+source("PeterTurnerCorrection.R")
+source("tau_correct.R")
+source("tau_correct_2015.R")
+source("convert_gases.R")
+source("convert_gases_2015.R")
+source("sensor_clean2.R")
+
+#exporting data
+source("write_shapefile.R")
+source("extract_flame.R")
+
+#mapping data
+source("Heat_Maps.R")
+source("Google_Heat_Maps_ColorBar.R")
+source("Google_Heat_Maps.R")
